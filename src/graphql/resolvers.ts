340eb2f8a5eb2d7812dd6fb65796f1f7966a504e
John Tucker <jtucker@appirio.com>
2019-02-21 15:18:14 -0500

    Finish

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,3 +1,4 @@
+import { InMemoryCache } from 'apollo-boost';
 import gql from 'graphql-tag';
 
 const query = gql`
@@ -6,24 +7,36 @@ const query = gql`
   }
 `;
 
+interface CounterData {
+  counter: number;
+}
+
 export default {
   Mutation: {
-    counterDecrement: (_: any, params: any, { cache }: any) => {
-      const { counter } = cache.readQuery({ query });
+    counterDecrement: (_: any, params: any, { cache }: { cache: InMemoryCache }) => {
+      const data = cache.readQuery<CounterData>({ query });
+      if (data === null) {
+        return;
+      }
+      const { counter } = data;
       const nextCounter = counter - 1;
-      const data = {
+      const nextData = {
         counter: nextCounter,
       };
-      cache.writeData({ data });
+      cache.writeData({ data: nextData });
       return nextCounter;
     },
-    counterIncrement: (_: any, params: any, { cache }: any) => {
-      const { counter } = cache.readQuery({ query });
+    counterIncrement: (_: any, params: any, { cache }: { cache: InMemoryCache }) => {
+      const data = cache.readQuery<CounterData>({ query });
+      if (data === null) {
+        return;
+      }
+      const { counter } = data;
       const nextCounter = counter + 1;
-      const data = {
+      const nextData = {
         counter: nextCounter,
       };
-      cache.writeData({ data });
+      cache.writeData({ data: nextData });
       return nextCounter;
     },
   },
