01a7c9c4feaa511b3f014aa147d72dcc6eb1a637
John Tucker <jtucker@appirio.com>
2019-02-21 13:50:26 -0500

    Props

--- src/components/App/Hello/HelloView.tsx
+++ src/components/App/Hello/HelloView.tsx
@@ -1,13 +1,13 @@
 import React, { PureComponent } from 'react';
 import { HelloData, Mutations } from '../Hello';
 
-interface HelloViewProps extends Mutations {
+interface Props extends Mutations {
   data?: HelloData;
   error: boolean;
   loading: boolean;
 }
 
-class HelloView extends PureComponent<HelloViewProps> {
+class HelloView extends PureComponent<Props> {
   public render() {
     const { counterDecrement, counterIncrement, error, loading, data } = this.props;
     if (loading) {
